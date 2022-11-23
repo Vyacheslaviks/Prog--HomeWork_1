aaaaae13b90aa51a000f87beda952d6e3353548f
Slava <slavanew98@mail.ru>
2022-11-23 20:55:54 +0300

    Exercise 1 - completed

--- Exc_1/Program.cs
+++ Exc_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите 2 числа: ");
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+
+int min = 0;
+int max = 0;
+
+if (first > second)
+{
+    max = first;
+    min = second;
+}
+else
+{
+    max = second;
+    min = first;
+}
+
+Console.Write("Максимальное из чисел: ");
+Console.WriteLine(max);
+Console.Write("Минимальное из чисел: ");
+Console.WriteLine(min);
