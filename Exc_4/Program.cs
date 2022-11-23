cbe66749a49e62dea65af8cf64c3d05f04910e7e
Slava <slavanew98@mail.ru>
2022-11-23 21:25:32 +0300

    Exercise 4 - completed

--- Exc_4/Program.cs
+++ Exc_4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные сила от 1 до N
+
+Console.WriteLine("Введите число: ");
+
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (N > 0)
+{
+    while (count <= N)
+    {
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count);
+        }
+
+        count++;
+    }
+}
+else
+{
+    while (count >= N)
+    {
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count);
+        }
+
+        count--;
+    }
+}
