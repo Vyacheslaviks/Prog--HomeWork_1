2894923c4bfc4068dbbdcc6a5beebda8afc13ee0
Slava <slavanew98@mail.ru>
2022-11-23 21:14:51 +0300

    Exercise 3 - completed

--- Exc_3/Program.cs
+++ Exc_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход приимает число и выдаёт - является ли оно чётным
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число НЕ четное");
+}
