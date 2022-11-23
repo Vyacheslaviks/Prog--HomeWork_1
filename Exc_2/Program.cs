b02141da5606b902803ab6a9e2ffb3b37fd46039
Slava <slavanew98@mail.ru>
2022-11-23 21:06:22 +0300

    Exrcise 2 - completed

--- Exc_2/Program.cs
+++ Exc_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход 3 числа и выдает максиальное из этих чисел
+
+Console.WriteLine("Введите 3 числа:");
+
+int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+
+int max = first;
+
+if (second > max)
+{
+    max = second;
+}
+if (third > max)
+{
+    max = third;
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
